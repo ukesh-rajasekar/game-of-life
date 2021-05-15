10792404dbec539ed2fed54d2930855d3167f67c
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-05-15 15:36:12 +1000

    display grids

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
+import { useState } from 'react';
+
+const numRows = 50;
+const numCols = 50;
 
 function App() {
-   return <div className='App'>Game of life</div>;
+   let rows = [];
+   for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+   }
+   const [grid, setGrid] = useState(rows);
+
+   console.log(grid);
+
+   return (
+      <div
+         className='App'
+         style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${numCols}, 20px)`,
+         }}
+      >
+         {grid.map((rows, i) =>
+            rows.map((cols, k) => (
+               <div
+                  key={`${i}-${k}`}
+                  style={{
+                     width: 20,
+                     height: 20,
+                     background: grid[i][k] ? 'green' : undefined,
+                     border: 'solid 1px black',
+                  }}
+               />
+            ))
+         )}
+      </div>
+   );
 }
 
 export default App;
